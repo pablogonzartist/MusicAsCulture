7482d6dcbc57a3d255c9db0573d08dea7557d3fa
pablogonzartist <gonzp390@newschool.edu>
2021-05-01 14:41:47 -0400

    page updates

--- script.js
+++ script.js
@@ -42,4 +42,39 @@ form.addEventListener('submit', function(e) {
        location.href=("pages/2020/2020.html");                    
        return false;
     }
+  
+    if (input == "2019"){
+      location.href=("pages/2019/2019.html");                    
+      return false;
+   }
+
+   if (input == "2018"){
+    location.href=("pages/2018/2018.html");                    
+    return false;
+ }
+   if (input == "2017"){
+    location.href=("pages/2017/2017.html");                    
+    return false;
+  }
+  if (input == "2016"){
+    location.href=("pages/2016/2016.html");                    
+    return false;
+  }
+  if (input == "2015"){
+    location.href=("pages/2015/2015.html");                    
+    return false;
+  }
+  if (input == "2014"){
+    location.href=("pages/2014/2014.html");                    
+    return false;
+  }
+  if (input == "2013"){
+    location.href=("pages/2013/2013.html");                    
+    return false;
+  }
+  if (input == "2012"){
+    location.href=("pages/2012/2012.html");                    
+    return false;
+  }
+
 })
